31936d278f9ff66e1f2eb01aa01d61bfb1c048a6
carlocodes <Gian Uy>
2024-01-16 15:13:01 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/clippedconfigservice/ClippedConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.clippedconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippedConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippedConfigServiceApplication.class, args);
+	}
+
+}
